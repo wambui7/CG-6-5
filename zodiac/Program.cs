9a0f94432d0e99cfb8d6e3dc49a061eb2d2196d4
wambui7 <shiko.sheila@gmail.com>
2018-08-17 18:46:54 -0500

    no changes

--- zodiac/Program.cs
+++ zodiac/Program.cs
@@ -6,7 +6,64 @@ namespace zodiac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("please enter your birthdate");
+
+            DateTime birthdate = DateTime.Parse(Console.ReadLine());
+
+            string sign = "blank";
+
+            if (birthdate.Month == 1)
+            {
+                if (birthdate.Day >= 20)
+                {
+                    sign = "Aquarius";
+                }
+                else if (birthdate.Day <= 19)
+                {
+                    sign = "Capricorn";
+                }
+                else if (birthdate.Month == 2)
+                {
+                    if (birthdate.Day >= 19)
+                    {
+                        sign = "Pisces";
+                    }
+                    else if (birthdate.Day >= 21)
+                    {
+                        sign = "Aries";
+                    }
+                }
+
+                string fortune = "blank";
+
+                if (sign == "Aquarius")
+                {
+                    fortune = "you will be a priest";
+                }
+
+                else if (sign == "Capricorn")
+                {
+                    fortune = "you will get married tomorrow";
+                }
+
+                else if (sign == "Pisces")
+                {
+                    fortune = "you will win lottery today";
+                }
+
+                else if (sign == "Aries")
+                {
+                    fortune = "you will have a baby soon";
+                }
+
+                Console.WriteLine("your birthdate is :+birthdate.AddYears(1).Day of the week");
+                Console.ReadLine();
+
+                if (sign == "capricorn" || "Pisces")
+                {
+                    Console.WriteLine("sign");
+                    Console.WriteLine)("fortune");
+                    Console.ReadLine();
+                }
     }
-}
+
